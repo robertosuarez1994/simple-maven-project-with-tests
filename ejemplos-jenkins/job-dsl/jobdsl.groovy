f413153189c16913fbfb76ac2e8ca4d717d07a55
Roberto Suarez <roberto.suarez@synapse.com>
2020-10-15 11:13:25 -0300

    cambio

--- ejemplos-jenkins/job-dsl/jobdsl.groovy
+++ ejemplos-jenkins/job-dsl/jobdsl.groovy
@@ -11,6 +11,24 @@ job_name_2 = "otro-pipeline-job-dsl"
 //}
 
 //prueba
+
+//pipelineJob(job_name_2) {
+//    definition {
+//        cpsScm {
+//            scm{
+//                git{
+//                    branch('*/master')
+/*                    remote{
+                        url('https://github.com/robertosuarez1994/simple-maven-project-with-tests.git')
+                    }
+                }
+                
+            }
+            script(readFileFromWorkspace('ejemplos-jenkins/job-dsl/pipeline-ejemplo.groovy'))
+        }
+    }
+}*/
+
 pipelineJob(job_name_2) {
     definition {
         cpsScm {
@@ -23,7 +41,18 @@ pipelineJob(job_name_2) {
                 }
                 
             }
-            script(readFileFromWorkspace('ejemplos-jenkins/job-dsl/pipeline-ejemplo.groovy'))
+            stage('pull repository') {
+                steps {
+                    echo '****haciendo pull de nuestro repositorio*****'
+                    git 'https://github.com/robertosuarez1994/simple-maven-project-with-tests.git'
+                }
+            }
+            stage('build') {
+                steps {
+                    echo '****haciendo el build de nuestro repositorio*****'
+                    sh 'mvn clean package'
+                }
+            }
         }
     }
 }
