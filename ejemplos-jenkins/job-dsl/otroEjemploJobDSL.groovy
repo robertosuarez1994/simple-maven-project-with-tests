58639edaaa8ad24e844fbc7602b97683177a2d79
Roberto Suarez <roberto.suarez@synapse.com>
2020-10-15 11:44:55 -0300

    add groovy file

---
+++ ejemplos-jenkins/job-dsl/otroEjemploJobDSL.groovy
@@ -0,0 +1,16 @@
+folder('jobDSL_folder') {
+    displayName('jobDSL_folder')
+    description('aca vamos a guardar los archivos generados')
+}
+
+job('jobDSL_folder') {    
+    scm {        
+        git('https://github.com/robertosuarez1994/simple-maven-project-with-tests.git')    
+    }
+    triggers {          
+        scm('H/5 * * * *')    
+    }                   
+    steps {        
+        shell 'mvn clean package'
+    }
+}
